bde845d6ae0dda2f67a026022dc6681c527d2c91
mac <mac@AlaaMacBookPro.local>
2019-07-21 12:21:39 +0200

    react exam

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../src/App.css';
+class App extends React.Component {
+  state = {
+    data: [],
+    isLoaded: false,
+    user: [],
+    userIsLoaded: false,
+  };
+  useFetch = () => {
+    fetch('http://uinames.com/api/?ext&amount=5')
+      .then(response => response.json())
+      .then(data => this.setState({ data, isLoaded: true }));
+  };
+  getUser(index) {
+    this.setState({ user: this.state.data[index], userIsLoaded: true });
+  }
+  render() {
+    return (
+      <div className="search-bar">
+        <div>
+          <button className="button" onClick={this.useFetch}>
+            Get Users
+          </button>
+        </div>
+        <b>
+          {this.state.isLoaded && (
+            <ul className="list-users">
+              {this.state.data.map((item, index) => (
+                <li onClick={() => this.getUser(index)} key={index}>
+                  {item.name}
+                </li>
+              ))}
+            </ul>
+          )}
+        </b>
+        {this.state.userIsLoaded && (
+          <div className="user-information">
+            <img className="user-image" src={this.state.user.photo} alt="userphoto" />
+            <ul>
+              <b>
+                <p className="user-name">
+                  {' '}
+                  {this.state.user.name} {this.state.user.surname}
+                </p>
+              </b>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+              <li>Email : {this.state.user.email}</li>
+              <br />
+              <li>Gender : {this.state.user.gender}</li>
+              <br />
+              <li>Age : {this.state.user.age}</li>
+              <br />
+              <li>Country of origin : {this.state.user.region}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
